62cd62aac699a872ef86a1d1714f1320743bbcac
davtamay <davtamay@gmail.com>
2017-08-15 03:05:17 -0500

    v0.77d
    -removed gvrvideoplayer.package and meta file as wll as barrier metal file because of warning or error

--- Assets/Scripts/Games/Wack/WackGameManager.cs
+++ Assets/Scripts/Games/Wack/WackGameManager.cs
@@ -7,33 +7,13 @@ using System.Collections.Generic;
 
 public class WackGameManager : MonoBehaviour {
 
-	public Difficulty difficulty = Difficulty.easy;
-
-	public GameObject easyWack;
-	public GameObject mediumWack;
-	public GameObject hardWack;
-
-	public List<int> myIndices;
 
 
 	public GameObject[] totalMoles;
 	public List<GameObject> activeMoles; 
 
-	private float timePassed;
-
-	public int amountOfMolesToEnable = 3;
-
-	public float mediumLevelTime;
-	public float hardLevelTime;
 
 
-	private bool isEasy;
-	private bool isMedium;
-
-	public Text coinText;
-	WackLookClick wackLookClick;
-
-	private bool isDone;
 
 	public static WackGameManager Instance
 	{ get { return instance; } }
@@ -54,167 +34,6 @@ public class WackGameManager : MonoBehaviour {
 	}
 
 
-	void Start(){
-	/*
-		wackLookClick = GetComponent<WackLookClick> ();
-	
-		SetDifficulty (Difficulty.easy);
-		isEasy = true;
-
-		GameController.Instance.TimeToAdd(ref isDone, mediumLevelTime);
-		StartCoroutine (UpdateGame());*/
-	}
-		
-	IEnumerator UpdateGame(){
-		yield return null;
-	/*
-		while (true) {
-			if (!GameController.Instance.Paused) { 
-				timePassed += Time.deltaTime;
-
-				if (timePassed > mediumLevelTime && isEasy) {
-					wackLookClick.TurnOffAllMoles(GameController.Instance.GetNewWaveTime);
-
-				yield return StartCoroutine (GameController.Instance.NewWave ());
-					GameController.Instance.TimeToAdd(ref isDone, hardLevelTime);
-
-					SetDifficulty (Difficulty.medium);
-					isMedium = true;
-					isEasy = false;
-				}
-
-				if (timePassed > hardLevelTime && isMedium) {
-					wackLookClick.TurnOffAllMoles(GameController.Instance.GetNewWaveTime);
-
-					yield return StartCoroutine (GameController.Instance.NewWave ());
-
-					//warningSign.SetActive (false);
-					SetDifficulty (Difficulty.hard);
-					isMedium = false;
-				}
-	
-			
-			}
-			yield return null;
-		}
-	}
-
-
-
-		
-
-	void SetDifficulty(Difficulty selectedDifficulty){
-
-		int DivideByDifficulty= totalMoles.Length / 3;
-
-	
-
-
-
-
-		switch (selectedDifficulty) {
-
-		case Difficulty.easy:
-
-			easyWack.SetActive (true);
-			mediumWack.SetActive (false);
-			hardWack.SetActive (false);
-
-		
-
-			for (int i = amountOfMolesToEnable; i < DivideByDifficulty; i++) {
-
-				int myIndexPull = Random.Range(0, DivideByDifficulty);
-
-
-					while (myIndices.Contains (myIndexPull)) {
-						
-						myIndexPull = Random.Range (0, DivideByDifficulty);
-						
-					}
-
-					myIndices.Add (myIndexPull);
-			
-					
-
-				}
-		
-
-				foreach (int ran in myIndices)
-				totalMoles [ran].transform.parent.gameObject.SetActive (false);
-
-
-
-			AddMolesToActiveList ();
-
-			break;
-
-			case Difficulty.medium:
-			
-			easyWack.SetActive(true);
-			mediumWack.SetActive(true);
-			hardWack.SetActive(false);
-
-			for (int i = amountOfMolesToEnable; i < DivideByDifficulty; i++) {
-
-				int myIndexPull = Random.Range(DivideByDifficulty, DivideByDifficulty *2 );
-
-
-				while (myIndices.Contains (myIndexPull)) {
-
-					myIndexPull = Random.Range (DivideByDifficulty, DivideByDifficulty *2);
-
-				}
-
-				myIndices.Add (myIndexPull);
-
-
-
-			}
-
-
-			foreach (int ran in myIndices)
-				totalMoles [ran].transform.parent.gameObject.SetActive (false);
-
-			AddMolesToActiveList ();
-
-			break;
-
-			case Difficulty.hard:
-
-			easyWack.SetActive(true);
-			mediumWack.SetActive(true);
-			hardWack.SetActive(true);
-
-
-			for (int i = amountOfMolesToEnable; i < DivideByDifficulty; i++) {
-
-				int myIndexPull = Random.Range(DivideByDifficulty *2, DivideByDifficulty *3 );
-
-
-				while (myIndices.Contains (myIndexPull)) {
-
-					myIndexPull = Random.Range (DivideByDifficulty*2, DivideByDifficulty *3);
-
-				}
-
-				myIndices.Add (myIndexPull);
-
-
-
-			}
-
-
-			foreach (int ran in myIndices)
-				totalMoles [ran].transform.parent.gameObject.SetActive (false);
-
-			AddMolesToActiveList ();
-
-			break;
-
-
-		}*/
-	}
 	//CheckifMolesAreActiveInHierarcy (AKA scene - taking into account parent active)
 
 	public void AddMolesToActiveList(){
@@ -229,5 +48,6 @@ public class WackGameManager : MonoBehaviour {
 				   }
 	
 		}
+		Debug.Log ("Number of active moles: " + WackGameManager.Instance.activeMoles.Count);
 	}
 }
