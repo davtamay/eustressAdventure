42386338cba817fa1e6ffda62d41f5e4d71010d5
davtamay <davtamay@gmail.com>
2017-10-17 17:58:25 -0500

    v0.83
    -adding mechanim supported maleHuman with blend shapes blend tree custumation. 
    
    -adding IK look at script
    
    -adding helicopter

---
+++ Assets/Animations/AnimBehaviorScripts/EyeIKControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeIKControl : MonoBehaviour {
+
+	private Animator thisAnimator;
+
+	public bool ikActive = false;
+	public Transform lookObj = null;
+	// Use this for initialization
+
+	void Start () {
+		thisAnimator = GetComponent<Animator> ();
+	}
+	
+	void OnAnimatorIK(){
+	
+		if (thisAnimator) {
+		
+		
+			if (ikActive) {
+			
+			
+				thisAnimator.SetLookAtWeight (1);
+				thisAnimator.SetLookAtPosition (lookObj.position);
+				//thisAnimator.SetIKRotation(
+				//thisAnimator.SetIKRotation(S
+					
+			
+			} else {
+			
+				thisAnimator.SetLookAtWeight (0);
+			
+			}
+		
+		
+		}
+	
+	
+	}
+}
